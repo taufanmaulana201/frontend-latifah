10a4e39a2f03eefbea7d03c13214919214497925
taufan <taufanmaulana201201@gmail.com>
2023-07-15 12:08:45 +0700

    fixng bug detail

--- src/pages/DetailLaporan.jsx
+++ src/pages/DetailLaporan.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 
 const DetailLaporan = () => {
   const { id } = useParams();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
 
   const navigate = useNavigate();
 
@@ -21,81 +21,126 @@ const DetailLaporan = () => {
     }
   }, [navigate]);
 
-  useEffect(() => {
-    const response = axios.get(
-      `https://latifah-backend-production.up.railway.app/invoice/${id}`
+  const getdata = async () => {
+    const res = await axios.get(
+      `https://latifah-backend-production.up.railway.app/invoice${id}`
     );
-    setData(response);
-    console.log("data", data);
-  }, [id, data]);
+    setData(res.data);
+  };
+  useEffect(() => {
+    getdata();
+  }, []);
 
+  console.log("data", data);
   return (
     <div>
-      <Link to={"/laporan"}>kembali</Link>
-      <h1 style={{ fontWeight: "bold", fontSize: "25px" }}>Detail Transaksi</h1>
-      <div style={{ width: "100%", marginTop: "20px" }}>
+      {data ? (
+        <div style={{ padding: "25px" }}>
+          <Link
+            className="is-info button"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100px",
+              height: "30px",
+              borderRadius: "2px",
+            }}
+            to={"/laporan"}
+          >
+            <span className="ml-1">Kembali</span>
+          </Link>
+          <h1
+            style={{ fontWeight: "bold", fontSize: "25px", marginTop: "25px" }}
+          >
+            Detail Transaksi
+          </h1>
+          <div style={{ width: "100%", marginTop: "20px" }}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                fontWeight: "bold",
+              }}
+            >
+              <div style={{ width: "13%", textAlign: "left" }}>
+                Kode Transaksi
+              </div>
+              <div style={{ width: "87%", textAlign: "left" }}>
+                : {data.kode}
+              </div>
+            </div>
+          </div>
+          <div style={{ width: "100%" }}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                fontWeight: "bold",
+              }}
+            >
+              <div style={{ width: "13%", textAlign: "left" }}>
+                Tanggal Transaksi
+              </div>
+              <div style={{ width: "87%", textAlign: "left" }}>
+                : {moment(data.createdAt).format("L")}
+              </div>
+            </div>
+          </div>
+          <div style={{ width: "100%" }}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                fontWeight: "bold",
+              }}
+            >
+              <div style={{ width: "13%", textAlign: "left" }}>
+                Total Transaksi
+              </div>
+              <div style={{ width: "87%", textAlign: "left" }}>
+                : Rp. {data.total}
+              </div>
+            </div>
+          </div>
+          <div style={{ textAlign: "left", marginTop: "25px" }}>
+            <table className="table is-striped is-fullwidth">
+              <thead style={{ background: "#eaeaea" }}>
+                <tr>
+                  <th>ID</th>
+                  <th>Barang</th>
+                  <th>Harga</th>
+                  <th>Qty</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.ProductInvoices.map((product, index) => (
+                  <tr key={product.uuid}>
+                    <td>{index + 1}</td>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td>{product.qty}</td>
+                    <td>{product.total}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ) : (
         <div
           style={{
             display: "flex",
+            justifyContent: "center",
             alignItems: "center",
+            width: "100%",
+            height: "100vh",
           }}
         >
-          <div style={{ width: "35%", textAlign: "left" }}>Kode Transaksi</div>
-          <div style={{ width: "75%", textAlign: "left" }}>: {data.kode}</div>
+          <p>Loading....</p>
         </div>
-      </div>
-      <div style={{ width: "100%" }}>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ width: "35%", textAlign: "left" }}>
-            Tanggal Transaksi
-          </div>
-          <div style={{ width: "75%", textAlign: "left" }}>
-            : {moment(data.createdAt).format("L")}
-          </div>
-        </div>
-      </div>
-      <div style={{ width: "100%" }}>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ width: "35%", textAlign: "left" }}>Total Transaksi</div>
-          <div style={{ width: "75%", textAlign: "left" }}>
-            : Rp. {data.total}
-          </div>
-        </div>
-      </div>
-      <div style={{ textAlign: "left", marginTop: "25px" }}>
-        <table className="table is-striped is-fullwidth">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Barang</th>
-              <th>Harga</th>
-              <th>Qty</th>
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.ProductInvoices.map((product, index) => (
-              <tr key={product.uuid}>
-                <td>{index + 1}</td>
-                <td>{product.name}</td>
-                <td>{product.price}</td>
-                <td>{product.qty}</td>
-                <td>{product.total}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      )}
     </div>
   );
 };
